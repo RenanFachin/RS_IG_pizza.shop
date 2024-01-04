b5df75d40774c52bac3339a3b341b9c65a07ed03
Renan <renan.fachin@hotmail.com>
2024-01-03 23:49:19 -0300

    refactor: create order-table-row component

---
+++ pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,50 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+
+          {/* Acessibilidade para leitores de tela */}
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+
+      <TableCell className="font-mono text-xs font-medium">
+        3da73ecf-413a-47b6
+      </TableCell>
+
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-300" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+
+      <TableCell className="font-medium">Renan Fachin</TableCell>
+
+      <TableCell className="font-medium">R$ 149,90</TableCell>
+
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
