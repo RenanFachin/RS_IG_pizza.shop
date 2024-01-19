4335f23cfcd3c5b9b2401be1b64d7bed08a058c8
Renan <renan.fachin@hotmail.com>
2024-01-19 15:47:38 -0300

    feat: Implementa a listagem de pedidos a partir de requisições ao back-end

---
+++ pizzashop-web/src/pages/app/orders/order-status.tsx
@@ -0,0 +1,45 @@
+type OrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+interface OrderStuatusProps {
+  status: OrderStatus
+}
+
+// Mapeando o retorno da API para português
+const orderStatusMap: Record<OrderStatus, string> = {
+  pending: 'Pendente',
+  canceled: 'Cancelado',
+  delivered: 'Entregue',
+  delivering: 'Em entrega',
+  processing: 'Em preparo',
+}
+
+export function OrderStatus({ status }: OrderStuatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      {status === 'pending' && (
+        <span className="h-2 w-2 rounded-full bg-slate-300" />
+      )}
+
+      {status === 'canceled' && (
+        <span className="h-2 w-2 rounded-full bg-rose-500" />
+      )}
+
+      {status === 'delivered' && (
+        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+      )}
+
+      {['processing', 'delivering'].includes(status) && (
+        <span className="h-2 w-2 rounded-full bg-amber-500" />
+      )}
+
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
+}
