2dcf8abe71bbb458aca40f8ea79a48b64e7a335d
Renan <renan.fachin@hotmail.com>
2024-01-20 21:01:11 -0300

    refactor: Alterando para um controlled component o modal de detalhes de pedido - evitando requisições desnecessárias para API

--- pizzashop-web/src/pages/app/orders/order-table-row.tsx
+++ pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -1,6 +1,7 @@
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { ArrowRight, Search, X } from 'lucide-react'
+import { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
@@ -20,10 +21,12 @@ export interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+
   return (
     <TableRow>
       <TableCell>
-        <Dialog>
+        <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
           <DialogTrigger asChild>
             <Button variant="outline" size="xs">
               <Search className="h-3 w-3" />
@@ -34,7 +37,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
           </DialogTrigger>
 
           {/* Conteúdo do dialog */}
-          <OrderDetails />
+          <OrderDetails orderId={order.orderId} />
         </Dialog>
       </TableCell>
 
