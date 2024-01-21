f52a2618716a2addf883641c194def77778f761c
Renan <renan.fachin@hotmail.com>
2024-01-21 17:41:24 -0300

    feat: Implementa o logout automático para usuários não autorizados

--- pizzashop-web/src/pages/_layouts/app.tsx
+++ pizzashop-web/src/pages/_layouts/app.tsx
@@ -1,8 +1,35 @@
-import { Outlet } from 'react-router-dom' // https://reactrouter.com/en/main/components/outlet
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom' // https://reactrouter.com/en/main/components/outlet
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      // Função disparada quando houver sucesso
+      (response) => response,
+      // Função disparada com erro
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
