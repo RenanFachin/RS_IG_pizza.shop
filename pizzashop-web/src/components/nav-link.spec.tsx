ffa1496c98e6616e3acb9ffeb961897201939a20
Renan <renan.fachin@hotmail.com>
2024-01-30 17:55:57 -0300

    test: Implementa teste unitário para componente Navink

---
+++ pizzashop-web/src/components/nav-link.spec.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { NavLink } from './nav-link'
+
+describe('NavLink', () => {
+  it('should highlight the nav link when is the current page link', () => {
+    const wrapper = render(
+      <>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </>,
+      {
+        // O componente NavLink funciona dentro de um contexto do router, é necessário criar um
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={['/about']}>{children}</MemoryRouter>
+          )
+        },
+      },
+    )
+
+    expect(wrapper.getByText('Home').dataset.current).toEqual('false')
+    expect(wrapper.getByText('About').dataset.current).toEqual('true')
+  })
+})
