86fda4939ae9cff7aec73aaeb92ef2a9a7aca3bd
Renan <renan.fachin@hotmail.com>
2024-01-30 15:43:29 -0300

    test: Configuração de ambiente de testes

--- pizzashop-web/vite.config.ts
+++ pizzashop-web/vite.config.ts
@@ -1,7 +1,9 @@
 import path from 'node:path'
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +13,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
