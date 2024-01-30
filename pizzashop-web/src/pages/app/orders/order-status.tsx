af22250d632a6608f68791286b4c2e7077a29b77
Renan <renan.fachin@hotmail.com>
2024-01-30 16:44:46 -0300

    test: cria teste unitário para o componente OrderStatus

--- pizzashop-web/src/pages/app/orders/order-status.tsx
+++ pizzashop-web/src/pages/app/orders/order-status.tsx
@@ -22,19 +22,31 @@ export function OrderStatus({ status }: OrderStuatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-300" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-300"
+        />
       )}
 
       {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-rose-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-rose-500"
+        />
       )}
 
       {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-emerald-500"
+        />
       )}
 
       {['processing', 'delivering'].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
