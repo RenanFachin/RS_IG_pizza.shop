9670c76ab81ddeb34fba72c7ff8361601cf7d8b6
Renan <renan.fachin@hotmail.com>
2024-02-01 17:45:00 -0300

    test: Finaliza mock de autenticação (adiciona um usuário com permissão de login)

--- pizzashop-web/src/api/msw/sign-in-mock.ts
+++ pizzashop-web/src/api/msw/sign-in-mock.ts
@@ -1,5 +1,24 @@
 import { http, HttpResponse } from 'msw'
 
-export const signInMock = http.post('/authenticate', () => {
-  return new HttpResponse(null, { status: 401 })
-})
+import { SignInBody } from '../sign-in'
+
+export const signInMock = http.post<never, SignInBody>(
+  '/authenticate',
+  async ({ request }) => {
+    // obtendo o body da requisição (é uma promisse)
+    const { email } = await request.json()
+
+    // Criando um "teste" para verificar o email digitado e criando uma autenticação de sucesso
+    if (email === 'johndoe@example.com') {
+      return new HttpResponse(null, {
+        status: 200,
+        // preenchendo o cookie (facilita os testes)
+        headers: {
+          'Set-Cookie': 'auth=sample-jwt',
+        },
+      })
+    }
+
+    return new HttpResponse(null, { status: 401 })
+  },
+)
