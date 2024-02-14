6aa8e3760692097c5f051019a55c545e50e1c533
Renan <renan.fachin@hotmail.com>
2024-02-14 20:48:46 -0300

    feat:Cria rota de criação de restaurant

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,9 +1,30 @@
 import { Elysia } from 'elysia' // Elysia é uma classe, precisamos instanciar
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
 const app = new Elysia()
 
-app.get('/', () => {
-  return 'Hello world from BUN!'
+app.post('/restaurants', async ({ body, set }) => {
+  const { restaurantName, name, email, phone } = body as any
+
+  const [manager] = await db
+    .insert(users)
+    .values({
+      name,
+      email,
+      phone,
+      role: 'manager',
+    })
+    .returning({
+      id: users.id,
+    })
+
+  await db.insert(restaurants).values({
+    name: restaurantName,
+    managerId: manager.id,
+  })
+
+  set.status = 204
 })
 
 app.listen(3333, () => {
