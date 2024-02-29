0a950a4e3e3c55b9559ed71f84e988312a2d8518
Renan <renan.fachin@hotmail.com>
2024-02-29 18:54:45 -0300

    config: Setup da autenticação utilizando JWT

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,8 +1,23 @@
-import { Elysia } from 'elysia' // Elysia é uma classe, precisamos instanciar
+import { Elysia, t } from 'elysia' // Elysia é uma classe, precisamos instanciar
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import cookie from '@elysiajs/cookie'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🛸 HTTP server running!')
