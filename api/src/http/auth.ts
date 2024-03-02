3156888c99fbd56241fd0e973054e0125577305a
Renan <renan.fachin@hotmail.com>
2024-03-02 00:56:28 -0300

    feat: Implementa tratamento de erros

--- api/src/http/auth.ts
+++ api/src/http/auth.ts
@@ -2,6 +2,7 @@ import cookie from '@elysiajs/cookie'
 import jwt from '@elysiajs/jwt'
 import { Elysia, t, type Static } from 'elysia'
 import { env } from '../env'
+import { UnauthorizedError } from './errors/unauthorized-error'
 
 const jwtPayload = t.Object({
   sub: t.String(),
@@ -9,6 +10,17 @@ const jwtPayload = t.Object({
 })
 
 export const auth = new Elysia()
+  .error({
+    UNAUTHORIZED: UnauthorizedError,
+  })
+  .onError(({ error, code, set }) => {
+    switch (code) {
+      case 'UNAUTHORIZED': {
+        set.status = 401
+        return { code, message: error.message }
+      }
+    }
+  })
   .use(
     jwt({
       secret: env.JWT_SECRET_KEY,
@@ -37,7 +49,7 @@ export const auth = new Elysia()
         const payload = await jwt.verify(cookie.auth)
 
         if (!payload) {
-          throw new Error('Unauthorized')
+          throw new UnauthorizedError()
         }
 
         return {
